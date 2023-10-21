1fb9b6d7872f6e0206d0e73369d5a5ccea13b83b
Himanshu631 <himanshupauljsr@gmail.com>
2023-10-21 17:50:28 +0530

    completed from 1st Lecture

---
+++ README.md
@@ -0,0 +1,3 @@
+My First Step to learn React.
+
+#Namaste React
---
+++ app.js
@@ -0,0 +1,45 @@
+// const heading = React.createElement("h1", 
+// {id:"heading"}, 
+// "Jai Hind from React"
+// );
+
+// console.log(heading);
+
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+
+// root.render(heading);
+
+const parent = React.createElement(
+    "div",
+    { id:"parent" }, 
+    [React.createElement(
+        "div",
+        { id:"child" },
+        [React.createElement(
+            "h1",
+            {},
+            "I am H1 Tag"),
+            React.createElement(
+                "h2",
+                {},
+                "I am H2 Tag")]
+    ),React.createElement(
+        "div",
+        { id:"child2" },
+        [React.createElement(
+            "h1",
+            {},
+            "I am H1 Tag"),
+            React.createElement(
+                "h2",
+                {},
+                "I am H2 Tag")]
+    )]
+);
+
+
+console.log(parent);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(parent);
